bfe6175fbf5e4e4527ad3f81638831ab381c0d77
Gael Thomas <gael.thomas@lip6.fr>
2011-05-24 00:00:17 +0200

    include error.h and do not redefine fatal

--- lib/p3/VMCore/P3Error.h
+++ lib/p3/VMCore/P3Error.h
@@ -3,11 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "debug.h"
 
 #define NI() \
 	do { fprintf(stderr, "%s is not implemented at %s::%d\n", __PRETTY_FUNCTION__, __FILE__, __LINE__); abort(); } while(0)
-
-#define fatal(...) \
-	do { fprintf(stderr, "FATAL: "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); abort(); } while(0)
 
 #endif
